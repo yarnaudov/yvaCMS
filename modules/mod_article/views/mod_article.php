4baab88b6256ccd99ca082ec594dfcfa3fbbe9fb
yarnaudov <yarnaudov@user27>
2013-06-17 09:25:42 +0300

    statistics test

--- modules/mod_article/views/mod_article.php
+++ modules/mod_article/views/mod_article.php
@@ -1,9 +1,11 @@
 
 <div>
 
+    <?php if($article['show_title'] == 'yes'){ ?>
     <div class="article_title" >
         <?=$article['title'];?>
     </div>
+    <?php } ?>
     
     <div class="article_content" >
         <?= $this->Article->parceText($article['text']);?>
