c4bf598fad42ed39794e85aa8cd4b362bebdc822
yarnaudov <yarnaudov@user27>
2013-08-29 16:11:27 +0300

    gallery module template

--- modules/mod_gallery/views/mod_gallery.php
+++ modules/mod_gallery/views/mod_gallery.php
@@ -2,29 +2,37 @@
 
 <?php if(count($albums) == 1){ ?>
     
+<ul>
+    
     <?php foreach($albums[0]['images'] as $image){ ?>
     
-    <div class="image_small">
+    <li>
         <a href="<?=site_url('gallery/image/'.$image['id'].'/600/600');?>">
             <img src="<?=$this->Image->getImageUrl($image['id'], 80, 80);?>" alt="<?=$image['title'];?>" >
         </a>
         <div class="description" ><?=$image['title'];?></div>
-    </div>
+    </li>
     
     <?php } ?>
 
+</ul>
+    
 <?php }else{ ?>
 
+<ul>
+    
     <?php foreach($albums as $album){
               $image = $this->Album->getImage($album['id']); ?>
     
-    <div class="image_small">
+    <li>
         <a href="<?=$menu_link;?>/album/<?=$album['id'];?>">
             <img src="<?=$this->Image->getImageUrl($image['id'], 150, 100);/*base_url('images/'.$image['id'].'.'.$image['ext']);*/?>" alt="<?=$album['title'];?>" >
         </a>
         <div class="description" ><?=$album['title'];?></div>
-    </div>
+    </li>
     
     <?php } ?>
+
+</ul>
 
 <?php } ?>
