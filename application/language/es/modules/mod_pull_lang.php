f4c555bf2cab2c5f80919d60d4de23a96975483b
yarnaudov <yarnaudov@user27>
2013-08-30 11:48:42 +0300

    add es to laguages

---
+++ application/language/es/modules/mod_pull_lang.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+$lang['label_mod_poll'] = 'Poll';
+$lang['label_mod_poll_please_wait']  = 'Please wait';
+$lang['label_mod_poll_back_to_poll'] = 'Back to poll';
+$lang['label_mod_poll_show_votes']   = 'Show votes';
+$lang['label_mod_poll_vote']         = 'Vote';
+
+
+$lang['label_mod_poll_total_votes']  = 'Total votes';
+$lang['label_mod_poll_you_already_voted_for'] = 'You have already voted for';
+
+?>
