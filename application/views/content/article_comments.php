4a80cc01634cac363d0ab416627abe4e0dfafadd
yarnaudov <yarnaudov@user27>
2013-10-08 15:35:37 +0300

    article comments check

--- application/views/content/article_comments.php
+++ application/views/content/article_comments.php
@@ -65,32 +65,26 @@
 
 	    </form>
 	    
-	    <script type="text/javascript" >
-		
-		$(document).ready(function() {
-		    
-		    $(".commentForm").validate({
+	    <?php 
+	    $script = "$('.commentForm').validate({
 			submitHandler: function(form) {
 
 			    var submit_form = true;
 
 			    if(form.ct_captcha){
 				submit_form = check_captcha(form);
-				//console.log(submit_form);
 			    }
 
 			    if(submit_form == true){
-				$(form).append('<input type="hidden" name="send" value="1" >');
-				//console.log('submit!');
+				$(form).append('<input type=\"hidden\" name=\"send\" value=\"1\" >');
 				form.submit();             
 			    }
 
 			}
-		    });
-		    
-		});
-		
-	    </script>
+			
+		       });";
+	    $this->jquery_ext->add_script($script);
+	    ?>
 	    
 	</div>
 	
