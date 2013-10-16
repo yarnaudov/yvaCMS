a7afeb8564274f0bb87773881091daac6e3f8e9f
yarnaudov <yarnaudov@user27>
2013-10-16 15:51:06 +0300

    settings file

--- settings.php
+++ settings.php
@@ -6,7 +6,8 @@
  *---------------------------------------------------------------
  *
  * Set administration panel folder name 
- * If you want different folder as admin, administrator etc. change line bellow
+ * If you want different folder as admin, administrator etc
+ * change line bellow and rename the folder!
  * 
  */
 define('APANEL_DIR', 'apanel');
@@ -21,6 +22,27 @@ define('APANEL_DIR', 'apanel');
  */
 define('TEMPLATES_DIR', 'templates');
 
+
+/*
+ *---------------------------------------------------------------
+ * COMPONENTS FOLDER
+ *---------------------------------------------------------------
+ *
+ * Set components folder name 
+ * 
+ */
+define('COMPONENTS_DIR', 'components');
+
+/*
+ *---------------------------------------------------------------
+ * MODULES FOLDER
+ *---------------------------------------------------------------
+ *
+ * Set modiles folder name 
+ * 
+ */
+define('MODULES_DIR', 'modules');
+
 /*
  *---------------------------------------------------------------
  * DATABASE CONNECTION
@@ -28,7 +50,7 @@ define('TEMPLATES_DIR', 'templates');
  * 
  */
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'yvacms_new');
+define('DB_NAME', 'yvacms');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_TYPE', 'mysql');
+define('DB_TYPE', 'mysqli');
