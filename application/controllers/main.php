158955bdf902229b1c1400010a529b30519bcd1d
yarnaudov <yarnaudov@user27>
2013-10-25 13:26:01 +0300

    articles comments form server check

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -63,20 +63,19 @@ class Main extends MY_Controller {
         $this->output->enable_profiler(FALSE);
 		
         require_once BASEPATH . '../plugins/securimage/securimage.php';
+		$image = new Securimage(array('case_sensitive' => false));
 
-        $image = new Securimage();
-		
+		# check via normal request
 		if($code != false ){
-			
 			if ($image->check($code) == true) {
 				return true;
 			} else {
 				return false;
-			}
-			
+			}			
 		}
 		
-        if ($image->check($_POST['code']) == true) {
+		# check via ajax without chengin key if correct
+        if ($image->getCode() == strtolower($_POST['code'])) {
             echo 1;
         } else {
             echo lang('msg_captcha_code_err');
