d14942eac7f7d7471932d6eef3a1f75d77739dd9
Yordan Arnaudov <y_arnaudov@yahoo.com>
2013-10-29 13:56:51 +0200

    translate system lang to russian

--- application/language/ru/system_lang.php
+++ application/language/ru/system_lang.php
@@ -2,9 +2,17 @@
 
 $lang['label_read_more']      = 'Read more';
 $lang['label_read_more_hide'] = 'Hide text';
+$lang['label_name']           = 'Имя';
+$lang['label_comment']        = 'Комментарий';
+$lang['label_add_comment']    = 'Добавь коментарий';
+$lang['label_send']           = 'Отправить';
+$lang['label_clear']          = 'Стереть';
+$lang['label_enter_code']     = 'Введите код';
 
-$lang['msg_article_not_found'] = 'The article was not found';
-$lang['msg_article_not_selected'] = 'No article selected!';
+$lang['msg_article_not_found']     = 'The article was not found';
+$lang['msg_article_not_selected']  = 'No article selected!';
 $lang['msg_category_not_selected'] = 'No category selected!';
+$lang['msg_captcha_code_err']      = 'Неправильно введен код';
+$lang['msg_no_comments']           = 'Комментариев еще нет. Будьте первым.';
 
 ?>
