0f5c315605bbb149564d506d25c6e09f083e9212
Yordan Arnaudov <y_arnaudov@yahoo.com>
2013-10-30 11:53:30 +0200

    fix banner main menu

--- application/views/banners/main.php
+++ application/views/banners/main.php
@@ -1,11 +1,9 @@
-
 <div class="banner banner_<?php echo $banner['type']; ?>" >
     
-    <?php if(isset($menu['show_title']) &&  $banner['show_title'] == 'yes'){ ?>
+    <?php if(isset($banner['show_title']) &&  $banner['show_title'] == 'yes'){ ?>
     <h3 class="title" ><?php echo $banner['title']; ?></h3>
     <?php } ?>
 
-    <?php echo $content;?>
+    <div class="content" ><?php echo $content;?></div>
 
 </div>
-
