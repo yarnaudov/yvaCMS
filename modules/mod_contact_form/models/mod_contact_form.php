d813e5452f3a6b9fc6c27c46657cc30c61c6962a
yarnaudov <yarnaudov@user27>
2013-11-05 16:06:48 +0200

    contact forms module fix

--- modules/mod_contact_form/models/mod_contact_form.php
+++ modules/mod_contact_form/models/mod_contact_form.php
@@ -8,27 +8,28 @@ class mod_contact_form extends CI_Model{
 		$this->load->language('components/com_cf');
 		$this->load->library('form_validation');
 		$this->load->helper(array('form', 'url'));
+		$this->load->add_package_path(COMPONENTS_DIR.'/contact_forms/');
+		$this->load->model('Contact_forms');
+		$this->load->language('components/com_cf');
 		
 		if($this->input->get_post('contact_form_id')){
-			$contact_form_id = $this->input->get('contact_form_id');
+			$contact_form_id = $this->input->get_post('contact_form_id');
 		}
 		else{
 			$contact_form_id = $module['params']['contact_form_id'];
 		}
+		
+		$contact_form = $this->Contact_forms->getDetails($contact_form_id);
 
-		$this->load->add_package_path(COMPONENTS_DIR.'/contact_forms/');
-		$this->load->model('Contact_forms');
-		$this->load->language('components/com_cf');
-
-		if(isset($_POST['send'])){
-            $this->Contact_forms->send($contact_form_id);
+		if($this->input->post('send')){
+            $this->Contact_forms->send($contact_form);
         }
 
 		$this->jquery_ext->add_plugin('validation');
 		$this->jquery_ext->add_library('check_captcha.js');
         $this->jquery_ext->add_library('../components/contact_forms/js/contacts.js');
 	
-        $data['contact_form'] = $this->Contact_forms->getDetails($module['params']['contact_form_id']);
+        $data['contact_form'] = $contact_form;
                        
         return module::loadContent($module, $data);
 	  	
