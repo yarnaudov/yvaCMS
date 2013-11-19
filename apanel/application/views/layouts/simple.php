259345641d32032616a06d1a6e52cfb9db6bd7f5
yarnaudov <y_arnaudov@abv.bg>
2013-11-19 10:18:27 +0200

    fix login problem

--- apanel/application/views/layouts/simple.php
+++ apanel/application/views/layouts/simple.php
@@ -4,20 +4,10 @@
   
 <div id="main" >
                  
-    <?php if($_SESSION['user_id'] == ""){ ?>
-
-    <script type="text/javascript" >    
-        parent.location.reload();    
-    </script>
-      
-    <?php }else{ ?>
-                 
     <div id="content-simple" >
         <?php echo $content; ?>
     </div>
       
-    <?php } ?>
-  
 </div>
 
 <?php $this->load->view('footer'); ?>
