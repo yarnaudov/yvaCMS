052c666c36e2bfea1b8ec37d6d6a4b96a4d0efd1
yarnaudov <yarnaudov@ws-yarnaudov.sf.vayant.com>
2014-01-17 12:41:06 +0200

    Add image to mod custom menu

--- modules/mod_custom_menu/views/mod_custom_menu.php
+++ modules/mod_custom_menu/views/mod_custom_menu.php
@@ -6,6 +6,11 @@ foreach($menus as $menu){
 
     echo '<li '.($menu['class'] != '' ? 'class="'.$menu['class'].'"' : '').' >';
     echo '  <a href="'.$menu['link'].'" target="'.$menu['target'].'" ><span>'.$menu['title'].'</span></a>';
+    
+    if(!empty($menu['image'])){
+        echo "<img src=\"".base_url($menu['image'])."\" >\n";
+    }
+    
     echo '</li>';
 
 }
